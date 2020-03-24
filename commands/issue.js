dbee26b7b55348c41a9c909c1d38ce48b8e8faab
Lars Mueller <appgurulars@gmx.de>
2020-03-24 20:03:36 +0100

    Spaces to tabs

--- commands/issue.js
+++ commands/issue.js
@@ -1,7 +1,7 @@
 const {gitHubSearchCommand} = require("../common.js");
 module.exports = gitHubSearchCommand({
-    name: "issue",
-    aliases: ["bug", "problem", "request"],
-    usage: "<search term>",
-    description: "Search Minetest issues on GitHub",
+	name: "issue",
+	aliases: ["bug", "problem", "request"],
+	usage: "<search term>",
+	description: "Search Minetest issues on GitHub",
 }, "issue");
--- commands/pull.js
+++ commands/pull.js
@@ -1,7 +1,7 @@
 const {gitHubSearchCommand} = require("../common.js");
 module.exports = gitHubSearchCommand({
-    name: "pull",
-    aliases: ["pr"],
-    usage: "<search term>",
-    description: "Search Minetest pull requests on GitHub"
+	name: "pull",
+	aliases: ["pr"],
+	usage: "<search term>",
+	description: "Search Minetest pull requests on GitHub"
 }, "pr");
